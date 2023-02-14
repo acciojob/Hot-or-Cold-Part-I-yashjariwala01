d269d1f569c2325679c2471ddc28950f1e643649
yashjariwala01 <122197521+yashjariwala01@users.noreply.github.com>
2023-02-14 17:23:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-//your code here
+var buttone = document.getElementById("btn");
+var pTag = document.getElementById("num");
+var input;
+var respo = document.getElementById("respond");
+var inputValue= document.getElementById("guess").value;
 
+function generateRandomValue(){
+	var RanNo= Math.floor(Math.random()*41)-20;
+	document.getElementById("num").innerText =RanNo;
+	getGuess();
+	result();
+}
+function getGuess(){
+	input = Number(document.getElementById("guess").value);
+}
+function result(){
+	if(Math.abs(RanNo - input) <= 5){
+	document.getElementById("respond").innerText ="Hot";
+	}
+	else{
+	document.getElementById("respond").innerHTML ="Cold";
+	}
+}
+buttone.addEventListener("click", generateRandomValue)
+
+   
